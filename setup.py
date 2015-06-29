0ca3dc8402f004481d4cf6b439c0666b1417f535
Jonathan Heathcote <mail@jhnet.co.uk>
2015-06-29 18:26:39 +0100

    v0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rig-par-diagram",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
 
     # Metadata for PyPi
