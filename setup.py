111dd28a5c91d7ebac3ac0f9b2e49653f851e4f7
Jonathan Heathcote <mail@jhnet.co.uk>
2015-10-02 14:21:19 +0100

    v0.0.3
    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="rig-par-diagram",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
 
     # Metadata for PyPi
